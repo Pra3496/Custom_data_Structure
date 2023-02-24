289b49aee6f95c30db71f3671f878caff5a57019
Pranav Waghmare <piyushw55@gmail.com>
2023-02-24 19:56:55 +0530

    [Pranav]Add: Linked List and Add First Method

--- Data_Structures/MyLinkedList.cs
+++ Data_Structures/MyLinkedList.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Data_Structures
 {
@@ -25,13 +26,30 @@ namespace Data_Structures
 
                 while(temp.next != null)
                 {
-                    temp= temp.next;
+                    temp = temp.next;
                 }
-                   temp.next = node;
-                
-               
+
+                temp.next = node;
+                  
             }
             Console.WriteLine("{0} Inserted into Linked List", data);
+        }
+
+        public void addFirst(int data)
+        {
+            if (this.head == null)
+            {
+                add(data);
+            }
+            else
+            {
+                Node newNode = new Node(data);
+                newNode.next = head;
+                head = newNode;
+                Console.WriteLine("{0} is added into linked list", newNode.data);
+            }
+
+            
         }
 
         internal void display()
@@ -47,13 +65,13 @@ namespace Data_Structures
             {
                 Console.Write("Head-",temp.data);
 
-                while (temp.next != null)
+                while (temp != null)
                 {
-                    Console.Write("{0} -> ", temp.data);
+                    Console.Write(" {0} -> ", temp.data);
                     temp = temp.next;
                 }
 
-                Console.Write("[{0}]-NULL", temp.data);
+                Console.Write("-NULL");
             }
         }
         
--- Data_Structures/Program.cs
+++ Data_Structures/Program.cs
@@ -1,27 +1,67 @@
-﻿namespace Data_Structures
+﻿using System;
+
+namespace Data_Structures
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           LinkedList list = new LinkedList();
+           MyLinkedList list = new MyLinkedList();
 
-           list.add(70);
+            Console.WriteLine("Welcome to data structure programs");
+            
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Please enter an option");
+                Console.WriteLine("1 : Create linked list");
+                Console.WriteLine("2 : Display linked list");
+                Console.WriteLine("3 : Add First linked list");
+                Console.WriteLine("0 : Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        list.add(56);
+                        list.add(30);
+                        list.add(70);
+                        break;
+
+                    case 2:
+                        Console.WriteLine();
+                        list.display();
+                        Console.WriteLine();
+                        break;
+
+                    case 3:
+                        list.addFirst(70);
+                        list.addFirst(30);
+                        list.addFirst(56);
+                        break;
+
+                    case 0:
+                        flag= false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter right option");
+                        break;
+                }
+            }
+            /*
+            list.add(70);
             Console.WriteLine();
             list.display();
 
             Console.WriteLine();
 
             list.add(30);
-            Console.WriteLine();
-            list.display();
-
-            Console.WriteLine();
+            
 
             list.add(56);
             Console.WriteLine();
             list.display(); 
-
+            */
 
 
 
