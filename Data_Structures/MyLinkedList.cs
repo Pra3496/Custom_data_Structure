b60e0e344a4e121ffbcaa16f27f9ca05f2de2877
Pranav Waghmare <piyushw55@gmail.com>
2023-02-24 19:32:23 +0530

    [Pranav]Add: Create Linked List and Add Method

---
+++ Data_Structures/MyLinkedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures
+{
+    public class MyLinkedList
+    {
+        internal Node head;
+
+        public void add(int data)
+        {
+            Node node = new Node(data);
+
+            if(this.head == null)
+            {
+                
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+
+                while(temp.next != null)
+                {
+                    temp= temp.next;
+                }
+                   temp.next = node;
+                
+               
+            }
+            Console.WriteLine("{0} Inserted into Linked List", data);
+        }
+
+        internal void display()
+        {
+            Node temp = this.head;
+
+            if(temp == null)
+            {
+                Console.WriteLine("Linked list is Empty");
+                return;
+            }
+            else
+            {
+                Console.Write("Head-",temp.data);
+
+                while (temp.next != null)
+                {
+                    Console.Write("{0} -> ", temp.data);
+                    temp = temp.next;
+                }
+
+                Console.Write("[{0}]-NULL", temp.data);
+            }
+        }
+        
+    }
+}
---
+++ Data_Structures/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Data_Structures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           LinkedList list = new LinkedList();
+
+           list.add(70);
+            Console.WriteLine();
+            list.display();
+
+            Console.WriteLine();
+
+            list.add(30);
+            Console.WriteLine();
+            list.display();
+
+            Console.WriteLine();
+
+            list.add(56);
+            Console.WriteLine();
+            list.display(); 
+
+
+
+
+        }
+    }
+}
