49c4e469f23ce063e01cc4d2c9feb7b40a2b80b8
Pranav Waghmare <piyushw55@gmail.com>
2023-05-11 00:51:23 +0530

    [Pranav]Added: Ability to Create Ordered List

---
+++ Data_Structures/MainPannel.cs
@@ -0,0 +1,102 @@
+﻿using static System.Console;
+
+namespace Data_Structures
+{
+    internal class MainPannel
+    {
+        private int selectIndex;
+        private string[] option;
+        private string prompt = @"
+  _____        _           _____ _                   _                       
+ |  __ \      | |         / ____| |                 | |                      
+ | |  | | __ _| |_ __ _  | (___ | |_ _ __ _   _  ___| |_ _   _ _ __ ___  ___ 
+ | |  | |/ _` | __/ _` |  \___ \| __| '__| | | |/ __| __| | | | '__/ _ \/ __|
+ | |__| | (_| | || (_| |  ____) | |_| |  | |_| | (__| |_| |_| | | |  __/\__ \
+ |_____/ \__,_|\__\__,_| |_____/ \__|_|   \__,_|\___|\__|\__,_|_|  \___||___/
+                                                                               ";
+
+
+
+
+        public MainPannel(string[] opt)
+        {
+
+            option = opt;
+            selectIndex = 0;
+
+        }
+
+        private void DisplayOptions()
+        {
+            Console.ForegroundColor= ConsoleColor.Blue;
+            WriteLine(prompt);
+            ResetColor();
+            WriteLine("\t\t\t----------- OPTIONS -----------\n");
+
+            for (int i = 0; i < option.Length; i++)
+            {
+                string currentOpt = option[i];
+                string prifix;
+
+                if (i == selectIndex)
+                {
+                    prifix = "*";
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    prifix = " ";
+                    ForegroundColor = ConsoleColor.White;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+                Write("\t\t\t     ");
+                WriteLine($"{prifix}<< {currentOpt} >>    ");
+                ResetColor();
+
+            }
+
+
+        }
+
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+
+            do
+            {
+                Clear();
+
+                DisplayOptions();
+
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+
+                keyPressed = keyInfo.Key;
+
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    selectIndex--;
+                    if (selectIndex == -1)
+                    {
+                        selectIndex = option.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    selectIndex++;
+                    if (selectIndex == option.Length)
+                    {
+                        selectIndex = 0;
+                    }
+                }
+
+
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return selectIndex;
+        }
+
+
+    }
+}
---
+++ Data_Structures/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Data_Structures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] opt = {"LINKED LIST","   STACK   ","   QUEUE   ","   Exit    "};
+
+            MainPannel panl = new MainPannel(opt);
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.Clear();
+                Console.WriteLine("\n");
+                int option = panl.Run();
+                Console.WriteLine("\n***************************************************************************\n");
+                switch (option)
+                {
+                    case 0:
+
+                        MyLinkedList list = new MyLinkedList();
+
+                        list.Run();
+                   
+                        Console.Write("\nExit From Linked List, press any key.....");
+                        Console.ReadKey();
+                        Console.WriteLine();  
+                        break;
+
+                    case 1:
+
+                        Stacks stack= new Stacks();
+
+                        stack.Run();
+
+                        Console.Write("\nExit From Stack, press any key.....");
+                        Console.ReadKey();
+                        Console.WriteLine();
+                        break;
+
+                    case 2 :
+
+                        Queue queue= new Queue();
+
+                        queue.Run();
+
+                        Console.Write("\nExit From Queue, press any key.....");
+                        Console.ReadKey();
+                        Console.WriteLine();
+                        break;
+
+                    case 3:
+                        flag= false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter right option");
+                        break;
+                }
+            }
+           
+
+
+
+        }
+    }
+}
